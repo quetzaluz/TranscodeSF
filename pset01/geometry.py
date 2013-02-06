125dc08b167283f87531927e04ffa6f10f878c48
Cyd La Luz <quetzaluz@gmail.com>
2013-02-05 16:32:00 -0800

    Update to geometry.py, still working on geometry_test.py

--- pset01/geometry.py
+++ pset01/geometry.py
@@ -1,28 +1,62 @@
+import numbers
+    #### This is a work in progress!!! ####
 class  Box:
     def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        try:
-            self.width = width
-            self.height = height
-            if self.width < 0:
-                raise ValueError, "No such thing as a negative width value! Try again."
-            if self.height < 0:
-                raise ValueError, "No such thing as a negative height value! Try again."
-        finally:
-            print "Box " + str(self) + " created."
+    # The try/except below ensures that only integer values are used.  
+        parsedx = False
+        parsedy = False
+        parsedwid = False
+        parsedhei = False
+        while parsedx == False:
+            try:
+                self.x = int(x)
+                parsedx = True
+            except ValueError:
+                print "Invalid value for x!"
+                x = raw_input("Please enter an integer for x:")
+            else:
+                break
+        while parsedy == False:
+            try:
+                self.y = int(y)
+                parsedy = True
+            except ValueError:
+                print "Invalid value for y!"
+                y = raw_input("Please enter an integer for y:")
+        while parsedwid == False:
+            try:
+                self.width = int(width)
+                parsedwid = True
+            except ValueError:
+                print "Invalid value for width!"
+                width = raw_input("Please enter a positive integer for width:")
+        while parsedhei == False:
+            try:
+                self.height = int(height)
+                parsedhei = True
+            except ValueError:
+                print "Invalid value for height!"
+                height = raw_input("Please enter a positive integer for height:")
+        print "Box " + str(self) + " created."
+
     def top(self):
         return self.y
+    
     def bottom(self):
         return self.y + self.height
+    
     def left(self):
         return self.x
+    
     def right(self):
         return self.x + self.width
+    
     def area(self):
         return self.width*self.height
+    
     def __str__(self):
         return "(%s, %s) => (%s, %s)" % (self.left(), self.top(), self.right(), self.bottom())
+
     def is_intersecting(self, other):
         if self.left() > other.left() and self.left() < other.right():
             return True
@@ -72,59 +106,3 @@ class  Box:
                     print "Box " + str(self) + " is unchanged. This box"
                     print "doesn't intersect with box " + str(other) + "."
                     break
-print ">>> b = Box(2, 3, 8, 4)"
-b = Box(2, 3, 8, 4)
-print ">>> b1 = Box(0, 0, 10, 10)b = Box(2, 3, 8, 4)"
-b1 = Box(0, 0, 10, 10)
-print ">>> b2 = Box(2, 3, 20, 6)"
-b2 = Box(2, 3, 20, 6)
-print ">>> b3 = Box(10, 12, 3, 4)"
-b3 = Box(10, 12, 3, 4)
-print ">>> b.left()"
-print b.left()
-print ">>> b.right()"
-print b.right()
-print ">>> b.top()"
-print b.top()
-print ">>> b.bottom()"
-print b.bottom()
-print ">>> print b"
-print b
-print ">>> b.area()"
-print b.area()
-print ">>> b1 = Box(0, 0, 10, 10)"
-b1 = Box(0, 0, 10, 10)
-print ">>> b2 = Box(10, 4, 2, 3)"
-b2 = Box(10, 4, 2, 3)
-print ">>> b3 = Box(-5, 5, 7, 3)"
-b3 = Box(-5, 5, 7, 3)
-print ">>> empty = Box(5, 5, 0, 0)"
-empty = Box(5, 5, 0, 0)
-print ">>> b1.is_intersecting(b2)"
-print b1.is_intersecting(b2)  # adjacent boxes are not intersecting
-print ">>> b2.is_intersecting(b3)"
-print b2.is_intersecting(b3)
-print ">>> b1.is_intersecting(b3)"
-print b1.is_intersecting(b3)
-print ">>> b1.is_intersecting(empty)"
-print b1.is_intersecting(empty)  # empty boxes don't intersect anything
-print ">>> b1 = Box(0, 0, 10, 10)"
-b1 = Box(0, 0, 10, 10)
-print ">>> b2 = Box(2, 3, 20, 6)"
-b2 = Box(2, 3, 20, 6)
-print ">>> b3 = Box(10, 12, 3, 4)"
-b3 = Box(10, 12, 3, 4)
-print ">>> b1.intersect(b2)"
-b1.intersect(b2)
-print ">>> print b1"
-print b1
-print ">>> print b2"
-print b2  # b2 is unchanged
-print ">>> b2.intersect(b3)"
-b2.intersect(b3)
-print ">>> b2.area()"
-print b2.area()  # The x,y location of b2 are not important
-print "******************************************************"
-print "*    Optional: try creating a box with a negative    *"
-print "*    height or width value. An error will result.    *"
-print "******************************************************"
