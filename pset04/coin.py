a845625daf7bc2e8266d9e909a315b2c94fc8c54
Cyd La Luz <quetzaluz@gmail.com>
2013-02-21 16:47:32 -0800

    Added printing to coin.py

--- pset04/coin.py
+++ pset04/coin.py
@@ -63,6 +63,39 @@ class CoinPurse(object):
             return CoinPurse(self.quarters+other.quarters, self.dimes+other.dimes, self.nickels+other.nickels, self.pennies+other.pennies)
         else:
             NotImplemented
+        #It seems like I don't have to override __iadd__ for +=: the function
+        #works just fine with the above definitions.
+            
+    def __str__(self):
+        coin_dict = {"quarter": self.quarters, "dime": self.dimes, "nickel": self.nickels, "penny": self.pennies}
+        str_list = []
+        has_penny = 0
+        #Pennies are separated from the loop because if/else logic in the loop
+        #was making the penny string value append out of order.
+        for i in coin_dict:
+            if i == "penny":
+                has_penny = coin_dict[i]
+            if coin_dict[i] == 1:
+                if i != "penny":
+                    str_list.append("%s %s" % (coin_dict[i], i))
+            elif coin_dict[i] > 1:
+                if i != "penny":
+                    str_list.append("%s %ss" % (coin_dict[i], i))
+        if has_penny == 1:
+            str_list.append("%s penny" % self.pennies)
+        if has_penny > 1:
+            str_list.append("%s pennies" % self.pennies)
+        if len(str_list) == 4:
+            return "%s, %s, %s, and %s" % (str_list[0], str_list[1], str_list[2], str_list[3])
+        elif len(str_list) == 3:
+            return "%s, %s, and %s" % (str_list[0], str_list[1], str_list[2])
+        elif len(str_list) == 2:
+            return "%s and %s" % (str_list[0], str_list[1])
+        elif len(str_list) == 1:
+            return str_list[0]
+        elif str_list == 0:
+            return "There are no coins in this coin purse."
+        
 if __name__ == '__main__':
     x = CoinPurse(quarters=5, pennies=100)
     print "%0.2f should be 2.25" % x.amount()
@@ -87,3 +120,11 @@ if __name__ == '__main__':
 
     y += x
     print "y should now have 5.75 money (has %0.2f)" % y.amount()
+    print x
+    print y
+    print z
+    print combined
+    print CoinPurse(quarters = 1, dimes = 1, nickels = 1, pennies = 1)
+    
+
+    
