2c32fe63ecfc747f129058495fbdac4fe25c7d1f
Cyd La Luz <quetzaluz@gmail.com>
2013-03-04 12:55:06 -0800

    Made oscilloscope.gif a little smalle (it's still over 2 megs, sorry, can delete), linked help.html in index.html, changed webserver.py to send messages in HTML 1.0 rather than 1.1.

--- pset05/webserver.py
+++ pset05/webserver.py
@@ -17,18 +17,18 @@ class HttpRequestHandler(SocketServer.StreamRequestHandler):
                 pageref = "." + splitdata[1]
                 pagefile = open(pageref, "rb")
                 if pageref[-3:] == "htm" or pageref[-3] == "tml": 
-                    reply = "HTTP/1.1 200 OK\nContent-type: text/html\r\n\r\n"+ pagefile.read()
+                    reply = "HTTP/1.0 200 OK\nContent-type: text/html\r\n\r\n" + pagefile.read()
                 elif pageref[-3:] == "css":
-                    reply = "HTTP/1.1 200 OK\nContent-type: text/css\r\n\r\n" + pagefile.read()
+                    reply = "HTTP/1.0 200 OK\nContent-type: text/css\r\n\r\n" + pagefile.read()
                 else:
                     reply = pagefile.read()
                 self.request.sendall(reply)
             except:
-                self.request.sendall("HTTP/1.1 404 Not Found\nContent-type: text/html\r\n\r\n<html> <body> <h4> ERROR 404: File Not Found</h4></body></html>")
+                self.request.sendall("HTTP/1.0 404 Not Found\nContent-type: text/html\r\n\r\n<html> <body> <h4> ERROR 404: File Not Found</h4></body></html>")
         elif splitdata[0] != "GET":
-            self.request.sendall('HTTP/1.1 501 Not Implemented\nContent-type: text/html\r\n\r\n<html><body><h4> ERROR 501: Not Implemented</h4></body></html>')
+            self.request.sendall('HTTP/1.0 501 Not Implemented\nContent-type: text/html\r\n\r\n<html><body><h4> ERROR 501: Not Implemented</h4></body></html>')
         else:
-            self.request.sendall('HTTP/1.1 400 Bad Request\nContent-type: text/html\n<html><body><h4>ERROR 400: Bad Request \r\n\r\nContent-type: text/html\r\n\r\n</h4></body></html>')
+            self.request.sendall('HTTP/1.0 400 Bad Request\nContent-type: text/html\n<html><body><h4>ERROR 400: Bad Request \r\n\r\nContent-type: text/html\r\n\r\n</h4></body></html>')
         
 if __name__ == "__main__":
     # localhost is the name for the computer you are currently on.
