00d0edf985cbd5eeb203e4e769b84444237dfeec
Cyd La Luz <quetzaluz@gmail.com>
2013-03-22 19:43:37 -0700

    Much better line drawing functionality, working on other tools next.

--- pset07/paint.js
+++ pset07/paint.js
@@ -10,6 +10,8 @@ var COLORS = [
 var div_colors = document.getElementsByClassName("color");
 var pen_color = "black"; //default pen color, can change.
 var mouse_down = 0;
+var last_x;
+var last_y;
 
 // Globals for this file
 
@@ -40,36 +42,47 @@ function init() {
 }}
 
 
+//Below is a helper function for the event listening scripts below.
+function lastCoords(this_x, this_y) {
+  last_x = this_x;
+  last_y = this_y;
+	}
+
 function toolBarColors(e) {
   for (var color in COLORS) {
 		div_colors[color].style.border = "5px solid white";}
   e.target.style.border = "5px solid cyan";
-	pen_color = e.target.id
-	console.log("Pen color is now " + pen_color)
+	pen_color = e.target.id;
+	console.log("Pen color is now " + pen_color);
 }
 
 function startLine(e) {
 	console.log("Start Pen Line at " + (e.x - e.target.offsetLeft) + "," + (e.y - e.target.offsetTop));
 	mouse_down = 1;
+	ctx.beginPath();
 }
 
 function linePath(e) {
 	if(mouse_down === 0) {
-		console.log("Not Drawing")}
+		console.log("Not Drawing");}
 	if(mouse_down === 1) {
 		console.log("Drawing Pen Line.")
-		var mouse_x = e.x - e.target.offsetLeft
-		var mouse_y = e.y - e.target.offsetTop
+		var this_x = e.x - e.target.offsetLeft;
+		var this_y = e.y - e.target.offsetTop;
+		ctx.lineWidth = 3;
+		ctx.strokeStyle = pen_color;
 		ctx.beginPath();
-		ctx.moveTo(mouse_x, mouse_y);
-		ctx.arc(mouse_x, mouse_y, 3, Math.PI*2, true)
-		ctx.fillStyle = pen_color;
-		ctx.fill();
+		ctx.moveTo(last_x, last_y);
+		ctx.lineTo(this_x, this_y);
+		ctx.stroke();
+		lastCoords(this_x, this_y);
 	}
 }
 
 function endLine(e) {
-
+	ctx.closePath();
+	last_x = undefined;
+	last_y = undefined;
 	mouse_down = 0;
 	console.log("Ending Pen Line");
 }
