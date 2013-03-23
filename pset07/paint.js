0564c6034a787555e020ac6f4354ab57b21abd1a
Cyd La Luz <quetzaluz@gmail.com>
2013-03-23 10:34:20 -0700

    Fixed error that occurred when pen cursor goes off canvas with 'mouseout' event handler.

--- pset07/paint.js
+++ pset07/paint.js
@@ -1,30 +1,30 @@
 
 var WIDTH = 600;
 var HEIGHT = 400;
+var canvas;
+var ctx;
+var div_colors = document.getElementsByClassName("color");
+var pen_color = "black"; //default pen color, can change.
+var mouse_down = 0;
+var this_x;
+var this_y;
+var last_x;
+var last_y;
 
 var COLORS = [
   "red",  "orange", "yellow", "green",
   "blue", "indigo", "violet", "black"
 ];
 
-var div_colors = document.getElementsByClassName("color");
-var pen_color = "black"; //default pen color, can change.
-var mouse_down = 0;
-var last_x;
-var last_y;
-
-// Globals for this file
-
-
-
-var canvas;
-var ctx;
+var TOOLS = [
+	"pen", "rectangle"
+];
 
 function each(obj, f) {
   for (var key in obj) {
     if (obj.hasOwnProperty(key)) {
       f(obj[key], key);
-q    }
+    }
   }
 };
 
@@ -36,10 +36,13 @@ function init() {
   for (var color in COLORS) {
 		div_colors[color].addEventListener("click", toolBarColors);
 		div_colors[color].style.border = "5px solid white";
+	div_colors['black'].style.border = "5px solid cyan";
 	canvas.addEventListener('mousedown', startLine);
 	canvas.addEventListener('mouseup', endLine);
 	canvas.addEventListener('mousemove', linePath);
-}}
+	canvas.addEventListener('mouseout', offCanvas);
+	}
+}
 
 
 //Below is a helper function for the event listening scripts below.
@@ -48,10 +51,17 @@ function lastCoords(this_x, this_y) {
   last_y = this_y;
 	}
 
+function offCanvas(e) {
+	mouse_down = 0;
+	last_x = undefined;
+	last_y = undefined;
+	console.log("Off Canvas.")
+}
+
 function toolBarColors(e) {
-  for (var color in COLORS) {
-		div_colors[color].style.border = "5px solid white";}
-  e.target.style.border = "5px solid cyan";
+	for (var color in COLORS) {
+		div_colors[color].style.border = "5px solid white";} 
+	e.target.style.border = "5px solid cyan";
 	pen_color = e.target.id;
 	console.log("Pen color is now " + pen_color);
 }
@@ -63,24 +73,21 @@ function startLine(e) {
 }
 
 function linePath(e) {
-  var this_x = e.clientX + document.body.scrollLeft - e.target.offsetLeft;
-	var this_y = e.clientY + document.body.scrollTop - e.target.offsetTop;
+  this_x = e.clientX + document.body.scrollLeft - e.target.offsetLeft;
+	this_y = e.clientY + document.body.scrollTop - e.target.offsetTop;
 	if(mouse_down === 0) {
 		console.log("Not Drawing");}
 	if(mouse_down === 1) {
-		if(this_x < 0 || this_y < 0 || this_x > WIDTH || this_y > HEIGHT) {
-			mouse_down = 0;
-		}		
-		else {
-			console.log("Drawing Pen Line.")
-			ctx.lineWidth = 3;
-			ctx.strokeStyle = pen_color;
-			ctx.beginPath();
-			ctx.moveTo(last_x, last_y);
-			ctx.lineTo(this_x, this_y);
-			ctx.stroke();
-			lastCoords(this_x, this_y);
-	}}
+		e.preventDefault(); // Try to avoid error where dragging highlights elements.
+		console.log("Drawing Pen Line.")
+		ctx.lineWidth = 3;
+		ctx.strokeStyle = pen_color;
+		ctx.beginPath();
+		ctx.moveTo(last_x, last_y);
+		ctx.lineTo(this_x, this_y);
+		ctx.stroke();
+		lastCoords(this_x, this_y);
+	}
 }
 
 function endLine(e) {
